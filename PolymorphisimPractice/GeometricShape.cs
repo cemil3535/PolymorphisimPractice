e9945b13c22fc8a0cd06593a0b1e8088587f0df7
cemil3535 <cemilozcan35@gmail.com>
2024-08-11 23:00:46 +0300

    Add project files.

---
+++ PolymorphisimPractice/GeometricShape.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphisimPractice
+{
+    // Base GeometricShape class of definition
+
+    public class GeometricShape
+    {
+        // Property of definiton
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public virtual double AreaCalculate()
+        {
+
+            return Width * Height;
+        }
+
+
+
+        // Inheriting the square class from the geometrics class
+
+        public class Square : GeometricShape
+        {
+            // Constructor for Square 
+            public Square(double side)
+            {
+                Width = side;
+                Height = side;
+
+            }
+        }
+
+        // Inheriting the rectange class from the geometrics class
+
+        public class Rectangle : GeometricShape
+        {
+            // Constructor for  Rectangle
+            public Rectangle(double width, double height)
+            {
+                Width = width;
+                Height = height;
+
+            }
+        }
+
+        //Inheriting the righttriange class from the geometrics class
+
+        public class RightTriangle : GeometricShape
+        {
+            // Constructor for Right Triangle 
+
+            public RightTriangle(double width, double height)
+            {
+                Width = width;
+                Height = height;
+
+            }
+
+            public override double AreaCalculate()
+            {
+                return (Width * Height) / 2;
+            }
+        }
+
+    }
+}
---
+++ PolymorphisimPractice/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PolymorphisimPractice;
+using static PolymorphisimPractice.GeometricShape;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Object create and use polymorphisim
+        GeometricShape square = new Square(5);
+        square.AreaCalculate();
+        GeometricShape rectangle = new Rectangle(4,7); 
+        rectangle.AreaCalculate();
+        GeometricShape rightTriangle = new RightTriangle(6, 9);
+        rightTriangle.AreaCalculate();
+
+        // print area calculate
+        Console.WriteLine("Kare Alani: " + square.AreaCalculate());
+        Console.WriteLine("Dikdortgen Alani: " + rectangle.AreaCalculate());
+        Console.WriteLine("Dik Ucgen Alnai: " + rightTriangle.AreaCalculate());
+    }
+}
